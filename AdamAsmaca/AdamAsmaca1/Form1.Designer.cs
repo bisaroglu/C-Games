867d6e17537669eb21df471b225b87a90df9db71
Doğuş Bışaroğlu <147525950+bisaroglu@users.noreply.github.com>
2024-12-21 15:42:30 +0300

    Add files via upload

---
+++ AdamAsmaca/AdamAsmaca1/Form1.Designer.cs
@@ -0,0 +1,105 @@
+﻿namespace AdamAsmaca1
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///Gerekli tasarımcı değişkeni.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///Kullanılan tüm kaynakları temizleyin.
+        /// </summary>
+        ///<param name="disposing">yönetilen kaynaklar dispose edilmeliyse doğru; aksi halde yanlış.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer üretilen kod
+
+        /// <summary>
+        /// Tasarımcı desteği için gerekli metot - bu metodun 
+        ///içeriğini kod düzenleyici ile değiştirmeyin.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblWordDisplay = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.tbGuess = new System.Windows.Forms.TextBox();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // lblWordDisplay
+            // 
+            this.lblWordDisplay.Font = new System.Drawing.Font("Rockwell", 26.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblWordDisplay.Location = new System.Drawing.Point(11, 9);
+            this.lblWordDisplay.Name = "lblWordDisplay";
+            this.lblWordDisplay.Size = new System.Drawing.Size(524, 63);
+            this.lblWordDisplay.TabIndex = 0;
+            this.lblWordDisplay.Text = "Hangman";
+            this.lblWordDisplay.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.lblWordDisplay.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Rockwell", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.Location = new System.Drawing.Point(398, 375);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(138, 63);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Tahmin";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // tbGuess
+            // 
+            this.tbGuess.Font = new System.Drawing.Font("Rockwell", 26.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.tbGuess.Location = new System.Drawing.Point(19, 380);
+            this.tbGuess.Name = "tbGuess";
+            this.tbGuess.Size = new System.Drawing.Size(130, 49);
+            this.tbGuess.TabIndex = 2;
+            this.tbGuess.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.pictureBox1.Location = new System.Drawing.Point(19, 76);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(516, 283);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox1.TabIndex = 3;
+            this.pictureBox1.TabStop = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(548, 450);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.tbGuess);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.lblWordDisplay);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblWordDisplay;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox tbGuess;
+        private System.Windows.Forms.PictureBox pictureBox1;
+    }
+}
+
---
+++ AdamAsmaca/AdamAsmaca1/Form1.cs
@@ -0,0 +1,104 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace AdamAsmaca1
+{
+    public partial class Form1 : Form
+    {
+        #region Veriables
+        List<string> words = new List<string>
+        {
+            "kars",
+            "bursa",
+            "amasya",
+            "antalya",
+            "mardin",
+            "diyarbekir",
+            "hatay",
+            "kilis",
+            "izmir",
+            "yalova",
+            "izmit"
+        };
+        int incorrectGuess;
+        Random random;
+        string selectedWord;
+        char[] displayWord;
+        #endregion
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            incorrectGuess = 0;
+            random = new Random();
+            selectedWord = words[random.Next(words.Count)];
+            displayWord = new string('_', selectedWord.Length).ToCharArray();
+            string formattedDisplayWord = string.Join(" ", displayWord);
+            lblWordDisplay.Text = formattedDisplayWord;
+        }
+
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            char guess = tbGuess.Text.ToLower()[0];
+            bool correctGuess = false;
+            for (int i = 0; i < selectedWord.Length; i++)
+            {
+                if (selectedWord[i] == guess)
+                {
+                    displayWord[i] = guess;
+                    correctGuess = true;
+                }
+            }
+            lblWordDisplay.Text = string.Join(" ", displayWord);
+            if (!correctGuess)
+            {
+                UpdateHangmanImage();
+            }
+            if (lblWordDisplay.Text.Contains('_'))
+            {
+                
+            }
+        }
+
+        private void UpdateHangmanImage()
+        {
+            incorrectGuess++;
+            switch (incorrectGuess)
+            {
+                case 1:
+                    pictureBox1.Image = Properties.Resources.hangman_1;
+                    break;
+                case 2:
+                    pictureBox1.Image = Properties.Resources.hangman_2;
+                    break;
+                case 3:
+                    pictureBox1.Image = Properties.Resources.hangman_3;
+                    break;
+                case 4:
+                    pictureBox1.Image = Properties.Resources.hangman_4;
+                    break;
+                case 5:
+                    pictureBox1.Image = Properties.Resources.hangman_5;
+                    MessageBox.Show("Bilemediniz! Seçilen Kelime: " + selectedWord);
+                    Application.Restart();
+                    break;
+            }
+        }
+    }
+}
